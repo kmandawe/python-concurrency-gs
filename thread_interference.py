8766d432da2f129bdf24ef4e150f9b922d4ff027
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-05-01 15:27:13 +0800

    Thread Interference

---
+++ thread_interference.py
@@ -0,0 +1,21 @@
+import threading
+
+
+class BankAccount:
+    def __init__(self):
+        self.bal = 0
+
+    def deposit(self, amt):
+        balance = self.bal
+        self.bal = balance + amt
+
+    def withdraw(self, amt):
+        balance = self.bal
+        self.bal = balance - amt
+
+
+b = BankAccount()
+t1 = threading.Thread(target=b.deposit, args=(100,))
+t2 = threading.Thread(target=b.withdraw, args=(50,))
+t1.start()
+t2.start()
