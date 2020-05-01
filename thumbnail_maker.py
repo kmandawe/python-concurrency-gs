a28757b6175c03d20813dc0aa57507c3cc6f89e4
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-05-01 16:26:11 +0800

    Multiple Lock permits using Semaphore

--- thumbnail_maker.py
+++ thumbnail_maker.py
@@ -20,17 +20,20 @@ class ThumbnailMakerService(object):
         self.output_dir = self.home_dir + os.path.sep + 'outgoing'
         self.downloaded_bytes = 0
         self.dl_lock = threading.Lock()
+        max_concurrent_dl = 4
+        self.dl_sem = threading.Semaphore(max_concurrent_dl)
 
     def download_image(self, url):
         # download each image and save to input dir
-        logging.info("downloading image at URL " + url)
-        img_filename = urlparse(url).path.split('/')[-1]
-        dest_path = self.input_dir + os.path.sep + img_filename
-        urlretrieve(url, dest_path)
-        img_size = os.path.getsize(dest_path)
-        with self.dl_lock:
-            self.downloaded_bytes += img_size
-        logging.info("image [{} bytes] saved to {}".format(img_size, dest_path))
+        with self.dl_sem:
+            logging.info("downloading image at URL " + url)
+            img_filename = urlparse(url).path.split('/')[-1]
+            dest_path = self.input_dir + os.path.sep + img_filename
+            urlretrieve(url, dest_path)
+            img_size = os.path.getsize(dest_path)
+            with self.dl_lock:
+                self.downloaded_bytes += img_size
+            logging.info("image [{} bytes] saved to {}".format(img_size, dest_path))
 
     def download_images(self, img_url_list):
         # validate inputs
